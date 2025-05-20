6d6ba77067715629f7f41498f3b4ecf2d8f577bf
VivekSutar21 <viveksutar9071@gmail.com>
2025-05-20 22:30:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+function darkmode(){
+    document.getElementById("body").style.backgroundColor = "rgb(26, 25, 25)";
+}
+
+function lightmode(){
+    document.getElementById("body").style.backgroundColor = "white";
+}
+
+function myFunction(){
+    alert("Hello Vivek")
+    
+}
